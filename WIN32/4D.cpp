c4266ba12d8834cdfffee872427365db0a93ed11
Danny Milosavljevic <dannym@scratchpost.org>
2011-04-01 18:24:37 +0000

    WIN32: clean up main program.

--- WIN32/4D.cpp
+++ WIN32/4D.cpp
@@ -12,79 +12,11 @@
 static struct GUI::REPL* REPL;
 
 // Global Variables:
-HINSTANCE hInst;								// current instance
-TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
-TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+static HINSTANCE hInst;								// current instance
+static TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+//static TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
 
-// Forward declarations of functions included in this code module:
-BOOL             InitInstance(HINSTANCE, int);
-
-int APIENTRY _tWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
-{
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-
- 	// TODO: Place code here.
-	MSG msg;
-	HACCEL hAccelTable;
-
-	// Initialize global strings
-	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-	LoadString(hInstance, IDC_MY4D, szWindowClass, MAX_LOADSTRING);
-
-	// Perform application initialization:
-	if (!InitInstance (hInstance, nCmdShow))
-	{
-		return FALSE;
-	}
-
-	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY4D));
-
-	// Main message loop:
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		// TODO IsWindow ? 
-		if (!REPL || (/*!IsDialogMessage(REPL_get_window(REPL), &msg) && */!TranslateAccelerator(REPL_get_window(REPL), hAccelTable, &msg)))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-	}
-
-	return (int) msg.wParam;
-}
-
-
-
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
-//  COMMENTS:
-//
-//    This function and its usage are only necessary if you want this code
-//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
-//    function that was added to Windows 95. It is important to call this function
-//    so that the application will get 'well formed' small icons associated
-//    with it.
-//
-
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
+static BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
    HWND hWnd;
 
    hInst = hInstance; // Store instance handle in our global variable
@@ -92,28 +24,37 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    InitCommonControls();
    {INITCOMMONCONTROLSEX InitCtrls;
 	InitCtrls.dwSize = sizeof(InitCtrls);
-	// Set this to include all the common control classes you want to use
-	// in your application.
 	InitCtrls.dwICC = ICC_WIN95_CLASSES;
 	InitCommonControlsEx(&InitCtrls);
    }
-
    LoadLibrary(_T("RICHED32.DLL"));
-
-   /*hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }*/
    REPL = GUI::REPL_new(NULL);
    ShowWindow(REPL_get_window(REPL), nCmdShow);
    UpdateWindow(REPL_get_window(REPL));
-
    return TRUE;
 }
 
-void Repaint(HWND hwnd, PAINTSTRUCT* ps) {
+/*void Repaint(HWND hwnd, PAINTSTRUCT* ps) {
+}*/
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow) {
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	MSG msg;
+	HACCEL hAccelTable;
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	//LoadString(hInstance, IDC_MY4D, szWindowClass, MAX_LOADSTRING);
+	if (!InitInstance(hInstance, nCmdShow))
+		return(FALSE);
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY4D));
+	while(GetMessage(&msg, NULL, 0, 0)) {
+		// TODO IsWindow ? 
+		if (!REPL || (/*!IsDialogMessage(REPL_get_window(REPL), &msg) && */!TranslateAccelerator(REPL_get_window(REPL), hAccelTable, &msg))) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	return((int) msg.wParam);
 }
 
