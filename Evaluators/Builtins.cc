0f1b1bf2bad920967261b11eee16d12303879598
Danny Milosavljevic <dannym@scratchpost.org>
2011-04-16 23:22:14 +0000

    add 'procedure?' skeleton

--- Evaluators/Builtins.cc
+++ Evaluators/Builtins.cc
@@ -8,14 +8,27 @@ struct Quoter : Operation {
 	virtual bool eager_P(void) const;
 	virtual AST::Node* execute(AST::Node* argument);
 };
+struct ProcedureP : Operation {
+	virtual AST::Node* execute(AST::Node* argument);
+};
+
 bool Quoter::eager_P(void) const {
 	return(false);
 }
 AST::Node* Quoter::execute(AST::Node* argument) {
 	return(argument);
 }
-
+AST::Node* ProcedureP::execute(AST::Node* argument) {
+	if(argument != NULL && (dynamic_cast<Operation*>(argument) != NULL))
+		return(argument); /* FIXME return true or false */
+	else
+		return(NULL);
+}
 AST::Node* intern(int value) {
+	/* FIXME */
+	return(NULL);
+}
+AST::Node* intern(bool value) {
 	/* FIXME */
 	return(NULL);
 }
