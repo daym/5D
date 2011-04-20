30e16619960166e5bfb2b4da6fba382e349f1b34
Danny Milosavljevic <dannym@scratchpost.org>
2011-04-20 17:46:08 +0000

    WIN32: fix rich text handling up a bit.

--- GUI/WIN32REPL.cc
+++ GUI/WIN32REPL.cc
@@ -101,63 +101,9 @@ void SetDlgItemTextCXX(HWND dialog, int control, const std::wstring& value) {
 static void SetDialogFocus(HWND dialog, int control) {
 	SendMessage(dialog, WM_NEXTDLGCTL, (WPARAM) GetDlgItem(dialog, control), (LPARAM) TRUE); 
 }
-/*BOOL LoadTextFileToEdit(HWND hEdit, LPCTSTR pszFileName)
-{
-    HANDLE hFile;
-    BOOL bSuccess = FALSE;
-
-    hFile = CreateFile(pszFileName, GENERIC_READ, FILE_SHARE_READ, NULL,
-        OPEN_EXISTING, 0, NULL);
-    if(hFile != INVALID_HANDLE_VALUE) {
-        DWORD dwFileSize;
-        dwFileSize = GetFileSize(hFile, NULL);
-        if(dwFileSize != 0xFFFFFFFF) {
-            LPCWSTR pszFileText;
-            pszFileText = (LPCWSTR) GlobalAlloc(GPTR, dwFileSize + 1);
-            if(pszFileText != NULL) {
-                DWORD dwRead;
-                if(ReadFile(hFile, pszFileText, dwFileSize, &dwRead, NULL)) {
-                    pszFileText[dwFileSize] = 0; // Add null terminator
-                    if(SetWindowText(hEdit, pszFileText))
-                        bSuccess = TRUE; // It worked!
-                }
-                GlobalFree(pszFileText);
-            }
-        }
-        CloseHandle(hFile);
-    }
-    return bSuccess;
-}
-BOOL SaveTextFileFromEdit(HWND hEdit, LPCTSTR pszFileName)
-{
-    HANDLE hFile;
-    BOOL bSuccess = FALSE;
-    hFile = CreateFile(pszFileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    if(hFile != INVALID_HANDLE_VALUE) {
-        DWORD dwTextLength;
-        dwTextLength = GetWindowTextLength(hEdit);
-        // No need to bother if there's no text.
-        if(dwTextLength > 0) {
-            LPSTR pszText;
-            DWORD dwBufferSize = dwTextLength + 1;
-            pszText = GlobalAlloc(GPTR, dwBufferSize);
-            if(pszText != NULL) {
-                if(GetWindowText(hEdit, pszText, dwBufferSize)) {
-                    DWORD dwWritten;
-                    if(WriteFile(hFile, pszText, dwTextLength, &dwWritten, NULL))
-                        bSuccess = TRUE;
-                }
-                GlobalFree(pszText);
-            }
-        }
-        CloseHandle(hFile);
-    }
-    return bSuccess;
-}*/
-
 void ClearRichTextSelection(HWND control) {
 	int iTotalTextLength;
-	iTotalTextLength = GetWindowTextLength(control);
+	iTotalTextLength = GetWindowTextLength(control); // FIXME W
 	SendMessage(control, EM_SETSEL, (WPARAM)(int)iTotalTextLength, (LPARAM)(int)iTotalTextLength);
 }
 
@@ -312,8 +258,8 @@ static void REPL_find_text(struct REPL* self, const std::wstring& text, bool upw
 	int index;
 	FINDTEXTEXW range;
 	if(upwards) {
-		range.chrg.cpMin = 0; // FIXME curstart
-		range.chrg.cpMax = 0;
+		range.chrg.cpMin = 0;
+		range.chrg.cpMax = -1; // GetWindowTextLengthW(GetDlgItem(self->fSearchDialog, IDC_OUTPUT)); // FIXME curstart, W
 	} else {
 		range.chrg.cpMin = 0;
 		range.chrg.cpMax = -1;
