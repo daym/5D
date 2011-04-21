8ae933e375ce3bc76351b3699ecb971413639ede
Danny Milosavljevic <dannym@scratchpost.org>
2011-04-21 14:35:42 +0000

    * add gen_return

--- Compilers/X86/CodeGen.cc
+++ Compilers/X86/CodeGen.cc
@@ -7,6 +7,7 @@ unsigned CodeGen::get_size_in_bits(AST::Node* source) {
 }
 void CodeGen::gen_push(AST::Node* source) {
         /* FIXME push source */
+        //FF, 89, 8A, 8B, 8C, 8E, ...
 }
 void CodeGen::gen_stack_throwaway_bits(int bit_count) {
         /* FIXME add bit_count / 8, %esp */
@@ -17,5 +18,16 @@ void CodeGen::gen_call(AST::Node* destination) {
 void CodeGen::gen_load_register(AST::Symbol* register_, AST::Node* value) {
         /* FIXME load register with value */
 }
+void CodeGen::gen_enter_frame(void) {
+	/* FIXME push %ebp
+	         mov %esp, %ebp */
+}
+void CodeGen::gen_leave_frame(void) {
+	/* FIXME pop %ebp */
+}
+void CodeGen::gen_return(int bit_count) {
+	/* FIXME ret */
+}
+
 }; /* end namespace X86 */
 }; /* end namespace Compilers */
