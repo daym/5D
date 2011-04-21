2b92b18fe32bcea087c3980f61648eb91a79b476
Danny Milosavljevic <dannym@scratchpost.org>
2011-04-21 11:42:17 +0000

    * add X86 CodeGen (unfinished)

---
+++ Compilers/X86/CodeGen.cc
@@ -0,0 +1,18 @@
+#include "Compilers/X86/CodeGen"
+namespace Compilers {
+namespace X86 {
+unsigned CodeGen::get_size_in_bits(AST::Node* source) {
+        /* FIXME */
+        return(42);
+}
+void CodeGen::gen_push(AST::Node* source) {
+        /* FIXME push source */
+}
+void CodeGen::gen_stack_throwaway_bits(int bit_count) {
+        /* FIXME add bit_count / 8, %esp */
+}
+void CodeGen::gen_call(AST::Node* destination) {
+        /* FIXME call destination */
+}
+}; /* end namespace X86 */
+}; /* end namespace Compilers */
