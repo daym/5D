138b0a574d716aa83cc341bdc428139cc852fca3
Danny Milosavljevic <dannym@scratchpost.org>
2011-04-21 14:53:58 +0000

    * add ARM codegen.

---
+++ Compilers/ARM/CodeGen.cc
@@ -0,0 +1,31 @@
+#include "Compilers/ARM/CodeGen"
+namespace Compilers {
+namespace ARM {
+unsigned CodeGen::get_size_in_bits(AST::Node* source) {
+        /* TODO actually inspect source and return a decent size (however that would work...) */
+        return(32);
+}
+void CodeGen::gen_push(AST::Node* source) {
+        /* FIXME push source */
+}
+void CodeGen::gen_stack_throwaway_bits(int bit_count) {
+        /* FIXME add bit_count / 8, %esp */
+}
+void CodeGen::gen_call(AST::Node* destination) {
+        /* FIXME call destination */
+}
+void CodeGen::gen_load_register(AST::Symbol* register_, AST::Node* value) {
+        /* FIXME load register with value */
+}
+void CodeGen::gen_enter_frame(void) {
+	/* FIXME stm %lr */
+}
+void CodeGen::gen_leave_frame(void) {
+	/* FIXME ldm %lr */
+}
+void CodeGen::gen_return(int bit_count) {
+	/* FIXME mov %lr, %pc */
+}
+
+}; /* end namespace ARM */
+}; /* end namespace Compilers */
