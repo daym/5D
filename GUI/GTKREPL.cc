bd9b205fe23dea6a8cf0ab0085882c5a7ba2abc0
Danny Milosavljevic <dannym@scratchpost.org>
2011-04-28 23:30:10 +0000

    add file name to confirm-save dialog.

--- GUI/GTKREPL.cc
+++ GUI/GTKREPL.cc
@@ -825,7 +825,10 @@ void REPL_set_file_modified(struct REPL* self, bool value) {
 bool REPL_confirm_close(struct REPL* self) {
 	if(REPL_get_file_modified(self)) {
 		GtkDialog* dialog;
-		dialog = (GtkDialog*) gtk_message_dialog_new(GTK_WINDOW(REPL_get_widget(self)), GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_QUESTION, (GtkButtonsType) 0, "Environment has been modified. Save?");
+		char* file_name;
+		file_name = Config_get_environment_name(self->fConfig);
+		dialog = (GtkDialog*) gtk_message_dialog_new(GTK_WINDOW(REPL_get_widget(self)), GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_QUESTION, (GtkButtonsType) 0, "Environment has been modified. Save?\n%s", file_name ? file_name : "");
+		g_free(file_name);
 		gtk_dialog_add_buttons(dialog, GTK_STOCK_GO_BACK, GTK_RESPONSE_CANCEL, gettext("Don't Save"), GTK_RESPONSE_CLOSE, GTK_STOCK_SAVE, GTK_RESPONSE_OK, NULL);
 		gtk_dialog_set_default_response(dialog, GTK_RESPONSE_OK);
 		{
