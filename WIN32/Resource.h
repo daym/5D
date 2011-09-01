4ef121bc73af4f125adfeed0667d85aeb1bb0f18
Danny Milosavljevic <dannym@scratchpost.org>
2011-09-01 14:30:03 +0000

    * add "Resource.h".

---
+++ WIN32/Resource.h
@@ -0,0 +1,46 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 4D.rc
+//
+#define IDC_MYICON                      2
+#define IDD_MY4D_DIALOG                 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MY4D                        107
+#define IDI_SMALL                       108
+#define IDC_MY4D                        109
+#define IDR_MAINFRAME                   128
+#define IDD_REPL                        129
+#define IDD_CONFIRM_CLOSE               131
+#define IDD_SEARCH                      132
+#define IDC_OUTPUT                      1000
+#define IDC_ENVIRONMENT                 1001
+#define IDC_EXECUTE                     1002
+#define IDC_COMMAND_ENTRY               1005
+#define IDC_SEARCH_TERM                 1007
+#define IDC_SEARCH_UPWARDS              1008
+#define IDC_SEARCH_CASE_SENSITIVE       1009
+#define IDM_FILE_OPEN                   32778
+#define IDM_FILE_SAVE                   32779
+#define IDM_FILE_EXECUTE                32781
+#define IDM_EDIT_CUT                    32790
+#define IDM_EDIT_COPY                   32791
+#define IDM_EDIT_PASTE                  32792
+#define IDM_EDIT_FIND                   32793
+#define IDM_EDIT_FINDNEXT               32799
+#define IDM_FILE_SAVE_AS                32805
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32806
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
