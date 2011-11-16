3b794ec2cd0d2ca22bac52ba629d588bad45befe
Danny Milosavljevic <dannym@scratchpost.org>
2011-11-16 23:05:35 +0000

    fix tests

--- Scanners/test-MathParser.cc
+++ Scanners/test-MathParser.cc
@@ -10,13 +10,15 @@ You should have received a copy of the GNU General Public License along with thi
 #include <iostream>
 #include <string.h>
 #include "Scanners/MathParser"
+#include "Scanners/OperatorPrecedenceList"
 
 void test_expression(const char* source, const char* expected_tree) {
 	const char* buf = source;
 	using namespace Scanners;
 	MathParser parser;
 	//std::cout << source << std::endl;
-	AST::Node* result = parser.parse(fmemopen((void*) buf, strlen(buf), "r"));
+	parser.push(fmemopen((void*) buf, strlen(buf), "r"), 0);
+	AST::Node* result = parser.parse(new OperatorPrecedenceList());
 	if(result->str() != expected_tree) {
 		std::cerr << "error: input was " << source << "; expected " << expected_tree << " but got " << result->str() << std::endl;
 		abort();
@@ -24,17 +26,17 @@ void test_expression(const char* source, const char* expected_tree) {
 }
 
 int main() {
-	test_expression("2+3⋅5", "(+ 2 (* 3 5))");
-	test_expression("2⋅3+5", "(+ (* 2 3) 5)");
-	test_expression("2⋅x+5", "(+ (* 2 x) 5)");
-	test_expression("(2+3)⋅5", "(* (+ 2 3) 5)");
-	test_expression("(sin 2+3)⋅5", "(* (+ (sin 2) 3) 5)");
-	test_expression("(div 2 4 5+3)⋅5", "(* (+ (((div 2) 4) 5) 3) 5)");
+	test_expression("2+3⋅5", "((+ 2) ((* 3) 5))");
+	test_expression("2⋅3+5", "((+ ((* 2) 3)) 5)");
+	test_expression("2⋅x+5", "((+ ((* 2) x)) 5)");
+	test_expression("(2+3)⋅5", "((* ((+ 2) 3)) 5)");
+	test_expression("(sin 2+3)⋅5", "((* ((+ (sin 2)) 3)) 5)");
+	test_expression("(div 2 4 5+3)⋅5", "((* ((+ (((div 2) 4) 5)) 3)) 5)");
 	test_expression("\\x x", "(\\ x x)");
 	test_expression("(\\x x) 1", "((\\ x x) 1)");
-	test_expression("a⨯b", "(⨯ a b)");
-	test_expression("a⃗⨯b⃗", "(⨯ a⃗ b⃗)");
-	test_expression("2⋅f(x)", "(* 2 (f x))");
+	test_expression("a⨯b", "((⨯ a) b)");
+	test_expression("a⃗⨯b⃗", "((⨯ a⃗) b⃗)");
+	test_expression("2⋅f(x)", "((* 2) (f x))");
 	test_expression("cos cos x", "((cos cos) x)"); // well, it doesn't know that cos is a function.
 	return(0);
 }
