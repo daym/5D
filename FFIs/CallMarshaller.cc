1ed44e1e6d624bf12cdc0e20f5e26c91ffe47f0f
Danny Milosavljevic <dannym@scratchpost.org>
2011-11-26 10:20:23 +0000

    update FFIs

--- FFIs/CallMarshaller.cc
+++ FFIs/CallMarshaller.cc
@@ -4,8 +4,13 @@ namespace FFIs {
 
 CallMarshaller::CallMarshaller(AST::Symbol* resultSignature, AST::Symbol* parameterSignature) {
 	this->resultSignature = resultSignature;
-	this->parameterSignature = parameterSignature;
+	this->parameterSignature = parameterSignature; // plus the worldliness as the first char.
 }
+/* in order to get here, you do: (translateFFI 'i 'pii) 
+   The next argument is the actual CProcedure, whose call will be wrapped in our abstractions. 
+   Thus, for example int open(const char *pathname, int flags, mode_t mode); will be:
+     \world \pathname \flags \mode (#LLopen pathname flags mode):world:nil
+*/
 AST::Node* CallMarshaller::executeLowlevel(AST::Node* argument) {
 	return(NULL); /* FIXME */
 }
