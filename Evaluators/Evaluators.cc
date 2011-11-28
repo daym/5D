3137667e8decbbe8244c55d7b2e5e217e9514f4f
Danny Milosavljevic <dannym@scratchpost.org>
2011-11-28 18:43:27 +0000

    rename 'reduce' to 'simplify' for now since there is also a function 'reduce' doing something slightly different (within the language)

--- Evaluators/Evaluators.cc
+++ Evaluators/Evaluators.cc
@@ -163,7 +163,7 @@ static AST::Node* shift(AST::Node* argument, int index, AST::Node* term) {
 AST::Node* Reducer::execute(AST::Node* argument) {
 	return(argument);
 }
-REGISTER_STR(Reducer, return("reduce");)
+REGISTER_STR(Reducer, return("simplify");)
 Reducer reducer;
 static bool wants_its_argument_reduced_P(AST::Node* fn) {
 	Operation* fnOperation = dynamic_cast<Operation*>(fn);
