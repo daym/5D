e0be94d6a4cbb9fe5c7b58571a3710f8b09388e8
Danny Milosavljevic <dannym@scratchpost.org>
2011-12-15 17:50:24 +0000

    add REPL_set_shared_dir_by_executable

--- GUI/GTKGUI.cc
+++ GUI/GTKGUI.cc
@@ -8,6 +8,7 @@ You should have received a copy of the GNU General Public License along with thi
 #include <gtk/gtk.h>
 #include "GUI/GTKREPL"
 #include "GUI/GTKView"
+#include "REPL/REPL"
 using namespace GUI;
 
 static GtkWindow* REPL_window;
@@ -64,6 +65,8 @@ static GtkWindow* make_REPL_window(GtkWindow* parent, const char* source_file_na
 	return(GTK_WINDOW(REPL_get_widget(REPL)));
 }
 int main(int argc, char* argv[]) {
+	if(argc >= 1)
+		REPL_set_shared_dir_by_executable(argv[0]);
 	/*GtkWindow* view;*/
 	gtk_init(&argc, &argv);
 	/*view = make_view_window();*/
