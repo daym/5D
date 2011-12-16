065dad32e9b72d1fd0f97d6aedccf4e04c28aacc
Danny Milosavljevic <dannym@scratchpost.org>
2011-12-16 15:41:00 +0000

    * fix record alignment a bit

--- FFIs/Record.cc
+++ FFIs/Record.cc
@@ -1,4 +1,9 @@
 #include <sstream>
+#ifdef WIN32
+//typedef short uint16_t;
+#else
+#include <stdint.h>
+#endif
 #include "Evaluators/Evaluators"
 #include "FFIs/Record"
 
@@ -6,28 +11,15 @@ namespace FFIs {
 
 static size_t getSize(char c) {
 	switch(c) {
-	case 'b': return(1);
-	case 'h': return(2);
-	case 'i': return(4);
-	case 'f': return(4);
-#if sizeof(long) == 8
-	case 'd': return(8);
-	case 'D': return(16);
-	case 'l': return(8);
-	case 'p': return(8);
-	case 'P': return(8);
-#else /* 32 bit */
-	case 'p': return(4);
-	case 'P': return(4);
-	case 'l': return(4);
-#ifdef WIN32	
-	case 'd': return(8);
-	case 'D': return(16);
-#else
-	case 'd': return(8);
-	case 'D': return(16);
-#endif
-#endif
+	case 'b': return(sizeof(char));
+	case 'h': return(sizeof(uint16_t));
+	case 'i': return(sizeof(int));
+	case 'f': return(sizeof(float));
+	case 'd': return(sizeof(double));
+	case 'D': return(sizeof(long double));
+	case 'l': return(sizeof(long));
+	case 'p': return(sizeof(void*));
+	case 'P': return(sizeof(void*));
 	default: return(0); /* FIXME */
 	}
 }
@@ -37,24 +29,11 @@ static size_t getAlignment(char c) {
 	case 'h': return(2);
 	case 'i': return(4);
 	case 'f': return(4);
-#if sizeof(long) == 8
-	case 'd': return(8);
-	case 'D': return(16);
-	case 'l': return(8);
-	case 'p': return(8);
-	case 'P': return(8);
-#else /* 32 bit */
-	case 'p': return(4);
-	case 'P': return(4);
-	case 'l': return(4);
-#ifdef WIN32	
-	case 'd': return(8);
-	case 'D': return(8); // FIXME
-#else
-	case 'd': return(4); /* on Linux: 4; except when you specify -malign-double */
-	case 'D': return(4);
-#endif
-#endif
+	case 'd': return(sizeof(long) == 8 ? 8 : 8); // FIXME latter: 4 for Linux
+	case 'D': return(sizeof(long) == 8 ? 16 : 8); // FIXME latter: 4 for Linux
+	case 'l': return(sizeof(long));
+	case 'p': return(sizeof(long) == 8 ? 8 : 4);
+	case 'P': return(sizeof(long) == 8 ? 8 : 4);
 	default: return(0); /* FIXME */
 	}
 }
@@ -76,9 +55,9 @@ AST::Str* Record_pack(AST::Str* formatStr, AST::Node* data) {
 		size_t size = getSize(*format);
 		for(; size > 0; --size)
 			sst << '\0'; // FIXME the actual value.
-		padding = maxAlign - (offset & (maxAlign - 1)) = (-offset) & (maxAlign - 1);
-		//new_offset = (offset + align - 1) & ~(align - 1);
-		for(; padding > 0; --padding)
+		offset += size;
+		new_offset = (offset + maxAlign - 1) & ~(maxAlign - 1);
+		for(; offset < new_offset; ++offset)
 			sst << '\0';
 	}
 	std::string v = sst.str();
@@ -88,9 +67,12 @@ AST::Node* Record_unpack(AST::Str* formatStr, AST::Str* dataStr) {
 	if(formatStr == NULL)
 		throw Evaluators::EvaluationException("Record_unpack needs format string.");
 	const char* format = formatStr->text.c_str();
-	const char* codedData = data->text.c_str();
-	size_t remainderLen = data->text.length();
+	const char* codedData = dataStr->text.c_str();
+	size_t remainderLen = dataStr->text.length();
 	size_t maxAlign = 0;
+	size_t padding = 0;
+	size_t offset = 0;
+	size_t new_offset = 0;
 	for(const char* format = formatStr->text.c_str(); *format; ++format) {
 		size_t align = getAlignment(*format);
 		if(align > maxAlign)
@@ -102,10 +84,11 @@ AST::Node* Record_unpack(AST::Str* formatStr, AST::Str* dataStr) {
 			throw Evaluators::EvaluationException("Record_unpack: not enough coded data for format.");
 		codedData += size; // FIXME the actual value.
 		remainderLen -= size;
-		padding = maxAlign - (offset & (maxAlign - 1)) = (-offset) & (maxAlign - 1);
-		//new_offset = (offset + align - 1) & ~(align - 1);
-		if(remainderLen < padding)
+		offset += size;
+		new_offset = (offset + maxAlign - 1) & ~(maxAlign - 1);
+		if(remainderLen < new_offset - offset)
 			throw Evaluators::EvaluationException("Record_unpack: not enough coded data for format padding.");
+		offset = new_offset;
 		codedData += padding;
 		remainderLen -= padding;
 	}
