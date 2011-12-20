eaff36cdda9c36b5bbf1b3e72cafe1864cb265ad
Danny Milosavljevic <dannym@scratchpost.org>
2011-12-20 10:32:15 +0000

    fix GC GLib allocator

--- FFIs/GLibAllocator.c
+++ FFIs/GLibAllocator.c
@@ -2,12 +2,18 @@
 #include <gc/gc.h>
 #include "FFIs/GLibAllocator"
 
+static void* GCx_calloc(size_t nmemb, size_t size) {
+	size_t sz = nmemb * size; // FIXME handle overflow here.
+	return(GC_malloc(sz));
+}
+static void GCx_free(void* p) {
+}
 void GLibAllocator_init(void) {
 	GMemVTable vtable = {
 	.malloc = GC_malloc,
 	.realloc = GC_realloc,
-	.free = NULL,
-	.calloc = GC_malloc,
+	.free = GCx_free,
+	.calloc = GCx_calloc,
 	.try_malloc = NULL,
 	.try_realloc = NULL,
 	};
