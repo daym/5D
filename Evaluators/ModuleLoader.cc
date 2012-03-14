9c0de2a6137a82f7990b76e98ef90ba1f2e9c016
Danny Milosavljevic <dannym@scratchpost.org>
2012-03-14 09:56:09 +0000

    extract BuiltinSelector and move it to Evaluators

--- Evaluators/ModuleLoader.cc
+++ Evaluators/ModuleLoader.cc
@@ -52,8 +52,11 @@ static AST::Node* force_import_module(const char* filename) {
 			parser.push(input_file, 0, filename);
 			AST::Node* result = NULL;
 			result = parser.parse(default_operator_precedence_list(), Symbols::SlessEOFgreater);
+			result = Evaluators::close(Symbols::Squote, &Evaluators::Quoter, result); // module needs that, so provide it.
+			result = Evaluators::close(Symbols::Sdot, AST::makeAbstraction(Symbols::Sa, AST::makeAbstraction(Symbols::Sb, AST::makeApplication(Symbols::Sa, Symbols::Sb))), result); // TODO close
 			result = Evaluators::close(Symbols::Scolon, &Evaluators::Conser, result); // dispatch [] needs that, so provide it.
 			result = Evaluators::close(Symbols::SrequireModule, &RModuleLoader, result); // module needs that, so provide it.
+			result = Evaluators::close(Symbols::SBuiltins, &REPLB::BuiltinGetter);
 			// ?? result = Evaluators::close(Symbols::SdispatchModule, AST::makeAbstraction(Symbols::Sexports, AST::makeApplication(&Evaluators::ModuleDispatcher, AST::makeApplication(&Evaluators::ModuleBoxMaker, Symbols::Sexports))), result);
 			result = prepare_module(result);
 			//fprintf(stderr, "before reduce_module\n");
