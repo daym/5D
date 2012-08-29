060ac1711e92e3545ef46259c5dd0a0f67cdbc4c
Danny Milosavljevic <dannym@scratchpost.org>
2012-08-29 11:30:26 +0000

    add missing files

---
+++ Evaluators/Logic.cc
@@ -0,0 +1,65 @@
+#include "AST/AST"
+#include "Evaluators/Evaluators"
+#include "Evaluators/Logic"
+#include "Evaluators/Builtins"
+#include "Evaluators/ModuleLoader"
+
+namespace Evaluators {
+
+using namespace AST;
+#define fn AST::makeAbstraction
+#define call AST::makeApplication
+#define sy AST::symbolFromStr
+#define T sy("t")
+#define F sy("f")
+#define A sy("a")
+#define B sy("b")
+#define M sy("m")
+#define R sy("r")
+#define BER sy("ber")
+#define WORLD sy("world")
+/* TODO move ioValue, ioWorld from Runtime to here */
+#define ioValue sy("ioValue")
+#define ioWorld sy("ioWorld")
+#define IO(entry) Evaluators::get_module_entry_accessor("IO", entry)
+
+//Scanners::MathParser::parse_simple("(\\t (\\f t))", NULL));
+NodeT aTrue = annotate(fn(T, fn(F, T)));
+NodeT aFalse = annotate(fn(T, fn(F, F)));
+
+NodeT internNative(bool value) {
+	return(value ? aTrue : aFalse);
+}
+
+static AST::NodeT sequence2(AST::NodeT a, AST::NodeT b, AST::NodeT fallback) {
+/*
+let (;) := \m \ber \world
+        let r := (m world) in
+        ber (ioValue r) (ioWorld r)
+
+which is
+
+(\r ber (ioValue r) (ioWorld r)) (m world)
+*/
+	Evaluators::reduce(a);
+	return(Evaluators::reduce(b));
+}
+NodeT Sequencer = annotate(fn(M, fn(BER, fn(WORLD, 
+	call(fn(R, call(call(BER, call(IO(ioValue), R)), call(IO(ioWorld), R))), 
+		call(M, WORLD)))))); /* FIXME */
+
+// \a \b \t \f a (b t f) f)
+NodeT Ander = annotate(fn(A, fn(B, fn(T, fn(F, call(call(A, call(call(B, T), F)), F))))));
+
+// \a \b \t \f a t (b t f)
+NodeT Orer = annotate(fn(A, fn(B, fn(T, fn(F, call(call(A, T), call(call(B, T), F)))))));
+
+// \a \t \f a f t
+NodeT Noter = annotate(fn(A, fn(T, fn(F, call(call(A, F), T)))));
+
+NodeT Ifer = annotate(fn(A, A));
+NodeT Elser = annotate(fn(A, A)); /* actually \a \b a b */
+NodeT Elifer = annotate(fn(A, A)); /* actually \a \b a b */
+
+}; /* end namespace Evaluators */
+
