d7c06d225e5a543ebf33a4e2864ce1dc871b8f52
Danny Milosavljevic <dannym@scratchpost.org>
2012-09-29 12:45:34 +0000

    fix GTK GUI compilation after Logic_init addition

--- GUI/GTKGUI.cc
+++ GUI/GTKGUI.cc
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License along with thi
 #include "Evaluators/Logic"
 
 using namespace GUI;
+using namespace Evaluators;
 
 static GtkWindow* REPL_window;
 /*
