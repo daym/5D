f514f0fbf7779f4ad45a8d1a298bfde14b0df9e9
Danny Milosavljevic <dannym@scratchpost.org>
2012-12-31 09:11:48 +0000

    add ModuleSystem, Exports

---
+++ lib/Builtins/ModuleSystem/Exports.cc
@@ -0,0 +1,87 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <5D/Allocators>
+#include <5D/Values>
+#include <5D/Operations>
+#include "Values/Values"
+#include "Values/Symbols"
+/* TODO clean up */
+#include "Evaluators/Evaluators"
+#include "Evaluators/Builtins"
+
+namespace ModuleSystem {
+using namespace Values;
+using namespace Symbols;
+
+static NodeT makeEntry(NodeT name, void* fn) {
+	NodeT entry = makePair(name, makeBox(fn, name/*TODO replicate the entire accessor */));
+	return(entry);
+}
+static NodeT makeExportsFVA(const char* fmt, char* names, va_list ap) {
+	char buf[2049];
+	while(*names && (*names == ' ' || *names == '&'))
+		++names;
+	if(*names && *names != ',') {
+		char* x;
+		NodeT name;
+		x = strchr(names, ',');
+		if(!x)
+			x = names + strlen(names);
+		else
+			*x = 0;
+		if(snprintf(buf, 2048, fmt, names) == -1)
+			abort();
+		name = makeStr(buf);
+		names = x;
+		void* fn = va_arg(ap, void*);
+		return(makeCons(makeEntry(name, fn), makeExportsFVA(fmt, names, ap)));
+	} else
+		return(NULL);
+}
+/*NodeT makeReflector(NodeT entries) {
+	NodeT entry = get_cons_head(entries);
+	NodeT name = get_pair_fst(entry);
+	return(makeCons(name, makeReflector(get_cons_tail(entries))));
+}*/
+NodeT makeExportsQ(const char* names, ...) {
+	NodeT result;
+	va_list ap;
+	va_start(ap, names);
+	result = makeExportsFVA("%s", GC_strdup(names), ap);
+	va_end(ap);
+	//result = makeCons(makePair(Sexports, makeCons(Sexports, makeReflector(result))), result); // exports are automatcially added by ModuleSystem dispatcher
+	return(result);
+}
+NodeT makeExportsFQ(const char* fmt, const char* names, ...) {
+	NodeT result;
+	va_list ap;
+	va_start(ap, names);
+	result = makeExportsFVA(fmt, GC_strdup(names), ap);
+	va_end(ap);
+	//result = makeCons(makePair(Sexports, makeCons(Sexports, makeReflector(result))), result); // exports are automatcially added by ModuleSystem dispatcher
+	return(result);
+}
+
+/* and the entire thing in the language itself: */
+
+static NodeT buildHashExports(NodeT node) {
+	if(nil_P(node))
+		return(node);
+	NodeT head = get_cons_head(node);
+	NodeT tail = Evaluators::evaluateToCons(get_cons_tail(node));
+	NodeT result = makeApplication(makeApplication(&Evaluators::Pairer, makeApplication(&Evaluators::Quoter, head)), head);
+	return makeCons(result, buildHashExports(tail));
+}
+/*
+better:
+
+parseExports! = (\world
+	parseValue! world ;\value
+	return! (map (\item ((quote item), item)) (eval value [('(:), (:)) ('nil, nil) ('(,), (,)]))
+) in 
+*/
+
+DEFINE_SIMPLE_STRICT_OPERATION(HashExporter, buildHashExports(argument))
+REGISTER_BUILTIN(HashExporter, 1, 0, symbolFromStr("#exports"))
+
+};
---
+++ lib/Builtins/ModuleSystem/Modules.cc
@@ -0,0 +1,121 @@
+#include <5D/Operations>
+#include <5D/Values>
+#include <5D/FFIs>
+#include "ModuleSystem/Modules"
+#include "Evaluators/Evaluators"
+#include "Values/Symbols"
+#include "Evaluators/Builtins"
+#include "Evaluators/ModuleLoader"
+#include "Evaluators/BuiltinSelector"
+
+/* note that a module can load other modules, hence this has a dependency to the ModuleLoader (only for this reason) */
+
+namespace ModuleSystem {
+using namespace Values;
+using namespace Evaluators;
+static __inline NodeT access_module(NodeT fn, NodeT argument) {
+	NodeT body = get_curried_operation_argument(get_curried_operation_operation(fn));
+	NodeT result = Evaluators::reduce(makeApplication(body, argument));
+	return(result);
+}
+
+NodeT prepareModule(NodeT result) {
+	Symbols::initSymbols();
+	result = Evaluators::close(Symbols::Squote, &Evaluators::Quoter, result); // module needs that, so provide it.
+	result = Evaluators::close(Symbols::Sdot, makeAbstraction(Symbols::Sa, makeAbstraction(Symbols::Sb, makeApplication(Symbols::Sa, Symbols::Sb))), result);
+	result = Evaluators::close(Symbols::Shashexclam, makeAbstraction(Symbols::Sa, makeAbstraction(Symbols::Sb, Symbols::Sb)), result); // rem
+	result = Evaluators::close(Symbols::Scolon, &Evaluators::Conser, result); // dispatch [] needs that, so provide it.
+	result = Evaluators::close(Symbols::Scomma, &Evaluators::Pairer, result); // dispatch [] needs that, so provide it.
+	result = Evaluators::close(Symbols::Snil, NULL, result); // dispatch [] needs that, so provide it.
+	result = Evaluators::close(Symbols::SrequireModule, &ModuleLoader, result); // module needs that, so provide it. // TODO maybe use Builtins.requireModule (not sure whether that's useful)
+	result = Evaluators::close(Symbols::SBuiltins, &Evaluators::BuiltinGetter, result);
+	// ?? result = Evaluators::close(Symbols::SdispatchModule, makeAbstraction(Symbols::Sexports, makeApplication(&Evaluators::ModuleDispatcher, makeApplication(&Evaluators::ModuleBoxMaker, Symbols::Sexports))), result);
+	result = Evaluators::provide_dynamic_builtins(result);
+	result = Evaluators::annotate(result);
+	return(result);
+}
+DEFINE_FULL_OPERATION(Module, {
+	return(access_module(fn, argument));
+})
+REGISTER_BUILTIN(Module, 3, 1, symbolFromStr("requireModule"));
+static NodeT mapGetFst2(NodeT fallback, Cons* c) {
+	if(c == NULL) {
+		NodeT tail = fallback ? reduce(makeApplication(fallback, Symbols::Sexports)) : NULL;
+		return(tail);
+	} else
+		return(makeCons(Evaluators::get_pair_first(reduce(evaluateToCons(reduce(get_cons_head(c))))), mapGetFst2(fallback, evaluateToCons(get_cons_tail(c)))));
+}
+
+static NodeT dispatchModule(NodeT options, NodeT argument) {
+	/* parameters: <exports> <fallback> <key> 
+	               2         1          0*/
+	CXXArguments arguments = Evaluators::CXXfromArgumentsU(options, argument, 1);
+	CXXArguments::const_iterator iter = arguments.begin();
+	/*std::stringstream buffer;
+	std::string v;
+	int position = 0;
+	Formatters::Math::print_CXX(new Scanners::OperatorPrecedenceList(), buffer, position, iter->second, 0, false);
+	v = buffer.str();
+	printf("%s\n", v.c_str());*/
+	Box* mBox = dynamic_cast<Box*>(iter->second);
+	++iter;
+	NodeT fallback = iter->second;
+	++iter;
+	NodeT key = iter->second;
+	HashTable* m;
+	if(!mBox) {
+		throw Evaluators::EvaluationException("invalid symbol table entry (*)");
+		return(NULL);
+	}
+	void* pBox = Values::pointerFromNode(mBox);
+	if(dynamic_cast<HashTable*>((NodeT) pBox) == NULL) {
+		m = new (UseGC) HashTable;
+		for(Cons* table = (Cons*) pBox; table; table = Evaluators::evaluateToCons(table->tail)) {
+			std::string irv = Evaluators::str(table->head);
+			//printf("irv %s\n", irv.c_str());
+			Cons* entry = evaluateToCons(reduce(get_cons_head(table)));
+			//std::string v = str(entry);
+			//printf("=irv> %s\n", v.c_str());
+			NodeT x_key = reduce(Evaluators::get_pair_first(entry));
+			//std::string vkey = Evaluators::str(x_key);
+			//printf("=key> %s\n", vkey.c_str());
+			NodeT value = reduce(Evaluators::get_pair_second(entry));
+			const char* name = get_symbol1_name(x_key);
+			if(name && m->find(name) == m->end())
+				(*m)[name] = value;
+		}
+		Cons* table = (Cons*) pBox;
+		set_box_value(mBox, m);
+		(*m)["exports"] = makeCons(Symbols::Sexports, mapGetFst2(fallback, table));
+	}
+	m = (HashTable*) mBox->value;
+	//std::string vkey = Evaluators::str(key);
+	//printf("%s\n", vkey.c_str());
+	const char* name = get_symbol_name(key); 
+	if(name) {
+		/*HashTable::const_iterator b = m->begin();
+		HashTable::const_iterator e = m->end();
+		for(; b != e; ++b) {
+			printf("%s<\n", b->first);
+		}
+		printf("searching \"%s\"\n", s->name);*/
+		HashTable::const_iterator iter = m->find(name);
+		if(iter != m->end())
+			return(iter->second);
+		else {
+			if(fallback) { // this should always hold
+				return(reduce(makeApplication(fallback, key)));
+			} else { // this is a leftover
+				std::stringstream sst;
+				sst << "library does not contain '" << name;
+				std::string v = sst.str();
+				throw Evaluators::EvaluationException(GCx_strdup(v.c_str()));
+			}
+		}
+	} else
+		throw Evaluators::EvaluationException("not a symbol");
+	return(NULL);
+}
+DEFINE_FULL_OPERATION2(ModuleDispatcher, dispatchModule)
+REGISTER_BUILTIN(ModuleDispatcher, (-3), 1, symbolFromStr("dispatchModule"))
+};
