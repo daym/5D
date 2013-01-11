1d988c14b4ea584f9c6deebdd6e1390cd47024fe
Danny Milosavljevic <dannym@scratchpost.org>
2013-01-11 00:00:44 +0000

    hint on how to do very simple arg parsing (not precedence-aware, though)

--- design/shuntingyard.py
+++ design/shuntingyard.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sys
 import symbols
 from symbols import intern
-def parse(tokenizer, OPL, env):
+def parse(tokenizer, env):
 	"""
 	This works like the following:
 		#operators is a stack of operators. It is always kept in order of ascending precedence. 
@@ -11,56 +12,68 @@ def parse(tokenizer, OPL, env):
 		An operator is handled like this: if we try to push a new operator with lower precedence on top, it will keep popping (and emitting) existing operators off the operator stack until the invariant is satisfied. Then the operator is pushed on the operator stack.
 		A closing parenthesis is an "operator" which is not being put on the operator stack (it would do nothing anyway), otherwise handled like any other operator.
 	"""
+	OPL = env
 	error = env(intern("error"))
+	errorP = env(intern("error?"))
 	operatorP = OPL(intern("operator?"))
 	operatorArgcount = OPL(intern("operatorArgcount"))
+	operatorPrefixNeutral = OPL(intern("operatorPrefixNeutral"))
 	operators = [] # deferred operators
 	operatorLE = OPL(intern("operator<="))
 	openingParenP = OPL(intern("openingParen?"))
 	closingParenP = OPL(intern("closingParen?"))
 	openingParenOf = OPL(intern("openingParenOf"))
 	argcount = 0
-	fillcount = 0
 	# TODO check argcount and scream at the right moment if necessary
 	# TODO support two-argument prefix operators ("if", "let", "import", "\\")
+	# TODO in principle, there would also be non-associative operators like relational operators. We handle them as left-associative.
+	prevInput = intern("(")
 	for input in tokenizer:
 		# TODO right-associative operators, suffix operators, prefix operators
-		if operatorP(input):
-			if not openingParenP(input):
-				bClosingParen = closingParenP(input)
-				insideP = (lambda o: not openingParenP(o)) if bClosingParen else (lambda o: True)
-				while len(operators) > 0 and insideP(operators[-1]):
-					pendingOperator = operators[-1]
-					if operatorLE(input, pendingOperator):
-						argcount = operatorArgcount(pendingOperator)
-						if fillcount < argcount:
-							yield error("<%s-operands>" % (operators[-1],), "<too-little>")
-							return
-						fillcount -= argcount - 1 # 1 is the result slot
-						yield operators.pop()
-					else:
-						break
-				if bClosingParen: # TODO (5*)
-					if len(operators) > 0 and (operators[-1] is openingParenOf(input)):
-						operators.pop()
-					else: # unbalanced paren, wrong paren
-						yield error(openingParenOf(input), operators[-1].text if len(operators) > 0 else "<value>")
-						return
+		if openingParenP(input):
+			operators.append(input)
+		elif operatorP(input):
+			def unaryPrefixP(input):
+				argcount = operatorArgcount(input)
+				if argcount > 1:
+					neutral = operatorPrefixNeutral(input)
+					return not errorP(neutral)
 				else:
-					operators.append(input)
+					return argcount == 1
+			bUnary = False
+			if operatorP(prevInput): # special-case prefix operators
+				bUnary = True
+				if operatorArgcount(input) > 1:
+					neutral = operatorPrefixNeutral(input)
+					yield neutral
+					if errorP(neutral):
+						return
+			# else binary, postfix
+			bClosingParen = closingParenP(input)
+			insideP = (lambda o, bUnary=bUnary: not openingParenP(o) and (not bUnary or unaryPrefixP(o))) if bClosingParen else (lambda o, bUnary=bUnary: not bUnary or unaryPrefixP(o))
+			while len(operators) > 0 and insideP(operators[-1]):
+				pendingOperator = operators[-1]
+				if operatorLE(input, pendingOperator):
+					yield operators.pop()
+				else:
+					break
+			if bClosingParen: # TODO (5*)
+				if len(operators) > 0 and (operators[-1] is openingParenOf(input)):
+					operators.pop()
+				else: # unbalanced paren, wrong paren
+					yield error(openingParenOf(input), operators[-1].text if len(operators) > 0 else "<value>")
+					return
 			else:
-					operators.append(input)
+				operators.append(input)
 			argcount = operatorArgcount(input)
 		else:
 			yield input
-			fillcount += 1
+			if not operatorP(prevInput):
+				yield intern(" ")
+		prevInput = input if not closingParenP(input) else None
 	while len(operators) > 0:
-		argcount = operatorArgcount(operators[-1])
-		if fillcount < argcount:
-			yield error("<%s-operands>" % (operators[-1],), "<too-little>")
-			return
-		fillcount -= argcount - 1 # 1 is the result slot
 		yield operators.pop()
+	# TODO check that in the end there is exactly one item on the result stack
 if __name__ == "__main__":
 	import scanner
 	import StringIO as io
@@ -69,25 +82,108 @@ if __name__ == "__main__":
 			return "%r" % val
 		else:
 			return str(val)
-	def OPL(name):
+	def env(name):
 		level = {
 			intern("("): -1,
 			intern("{"): -1,
 			intern("["): -1,
-			intern("+"): 22,
-			intern("-"): 22,
+			intern("."): 32,
+			intern("_"): 31,
+			intern("^"): 31,
+			intern("**"): 30,
 			intern("*"): 29,
+			intern("⋅"): 29,
 			intern("/"): 29,
+			intern("⨯"): 28,
+			intern(":"): 27,
+			intern("'"): 26,
+			intern(" "): 24,
+			intern("++"): 23,
+			intern("+"): 22,
+			intern("‒"): 22,
+			intern("-"): 22,
+			intern("%"): 21,
+			intern("∩"): 16,
+			intern("∪"): 15,
+			intern("∈"): 14,
+			intern("⊂"): 14,
+			intern("⊃"): 14,
+			intern("⊆"): 14,
+			intern("⊇"): 14,
+			intern("="): 10,
+			intern("≟"): 10,
+			intern("/="): 10,
+			intern("<"): 9,
+			intern("<="): 9,
+			intern(">"): 9,
+			intern(">="): 9,
+			intern("≤"): 9,
+			intern("≥"): 9,
+			intern("&&"): 8,
+			intern("∧"): 8,
+			intern("||"): 7,
+			intern("∨"): 7,
+			intern(","): 6,
+			intern("$"): 5,
+			#intern("if"): 4,
+			intern("elif"): 4,
+			intern("else"): 4,
+			intern("|"): 3,
+			intern("=>"): 2,
+			intern(";"): 2,
+			intern("?;"): 2,
+			intern("\\"): 1,
+			intern("let"): 0,
+			intern("let!"): 0,
+			intern("import"): 0,
 			intern(")"): -1,
 			intern("}"): -1,
 			intern("]"): -1,
 		}
+		def errorP(node):
+			return isinstance(node, tuple) and len(node) > 0 and node[0] is intern("error")
+		def operatorPrefixNeutral(node):
+			return intern("0") if node is intern("-") else \
+			       env(intern("error"))("<prefix-operator>", str1(node))
 		def operatorP(node):
 			return node in level.keys() # is intern("+") or node is intern("-") or node is intern("*") or node is intern("/")
 		def operatorArgcount(node): # positive, > 1: left assoc, negative: right assoc. = 1: prefix, = -1: suffix
-			return 2
+			R = -2
+			N = 2 # FIXME
+			P = 1
+			S = -1
+			return R if node is intern("_") else \
+			       R if node is intern("^") else \
+			       R if node is intern("**") else \
+			       R if node is intern("⨯") else \
+			       R if node is intern(":") else \
+			       P if node is intern("'") else \
+			       N if node is intern("∈") else \
+			       N if node is intern("⊂") else \
+			       N if node is intern("⊃") else \
+			       N if node is intern("⊆") else \
+			       N if node is intern("⊇") else \
+			       N if node is intern("=") else \
+			       N if node is intern("≟") else \
+			       N if node is intern("/=") else \
+			       N if node is intern("<") else \
+			       N if node is intern("<=") else \
+			       N if node is intern(">") else \
+			       N if node is intern(">=") else \
+			       N if node is intern("≤") else \
+			       N if node is intern("≥") else \
+			       R if node is intern(",") else \
+			       R if node is intern("$") else \
+			       R if node is intern("elif") else \
+			       R if node is intern("else") else \
+			       R if node is intern(";") else \
+			       P if node is intern("\\") else \
+			       P if node is intern("let") else \
+			       P if node is intern("let!") else \
+			       P if node is intern("import") else \
+			       2
 		def operatorLE(a,b):
-			return level[a] <= level[b]
+			return level[a] < level[b] or (level[a] == level[b] and operatorArgcount(b) > 0) # latter: leave right-associative operators on stack if in doubt.
 		def openingParenP(node):
 			return node is intern("(") or node is intern("{") or node is intern("[")
 		def closingParenP(node):
@@ -95,6 +191,10 @@ if __name__ == "__main__":
 		def openingParenOf(node):
 			return intern("(") if node is intern(")") else intern("{") if node is intern("}") else intern("[") if node is intern("]") else intern("<none>")
 		#def closingParenOfP(node):
+		def shebang(v):
+			return None
+		def stringFromSymbol(v):
+			return v.text
 		if name == intern("operator?"):
 			return operatorP
 		elif name == intern("operatorArgcount"):
@@ -107,14 +207,9 @@ if __name__ == "__main__":
 			return closingParenP
 		elif name == intern("openingParenOf"):
 			return openingParenOf
-		else:
-			return env(intern("error"))("<envitem>", name)
-	def env(name):
-		def shebang(v):
-			return None
-		def stringFromSymbol(v):
-			return v.text
-		if name == intern("#!"):
+		elif name == intern("operatorPrefixNeutral"):
+			return operatorPrefixNeutral
+		elif name == intern("#!"):
 			return shebang
 		elif name == intern("stringFromSymbol"):
 			return stringFromSymbol
@@ -131,22 +226,24 @@ if __name__ == "__main__":
 					return intern("#f"), inputFile.read(1)
 				return (env(intern("error"))("<value>"), "")
 			return readHash
-		elif name == intern("error"):
+		elif name is intern("error"):
 			return lambda *args: (intern("error"), "expected \"%s\", got \"%s\"" % (args[0], args[1]))
+		elif name is intern("error?"):
+			return errorP
 		else:
+			#return env(intern("error"))("<envitem>", name)
 			return lambda *args: (env(intern("error"))("<envitem>", name), "")
 	print "---"
-	def errorP(node):
-		return isinstance(node, tuple) and len(node) > 0 and node[0] is intern("error")
+	errorP = env(intern("error?"))
 	def test1(text, expected):
-		got = [x for x in parse(scanner.tokenize(io.StringIO(text), env), OPL, env)]
+		got = [x for x in parse(scanner.tokenize(io.StringIO(text), env), env)]
 		print text, got
 		assert got == map(intern, expected)
 	def test1Error(text, expected):
 		f = io.StringIO(text)
-		got = [x for x in parse(scanner.tokenize(f, env), OPL, env) if errorP(x)]
+		got = [x for x in parse(scanner.tokenize(f, env), env) if errorP(x)]
 		assert(len(got) > 0)
-		print "position", f.tell(), got[-1]
+		print "positionx", f.tell(), got[-1], errorP(got[-1])
 	test1("2+3", ["2", "3", "+"])
 	test1("2+3*5", ["2", "3", "5", "*", "+"])
 	test1("2*3+5", ["2", "3", "*", "5", "+"])
@@ -154,6 +251,9 @@ if __name__ == "__main__":
 	test1("2*(3+(5+4)+2)", ["2", "3", "5", "4", "+", "+", "2", "+", "*"])
 	test1("2*3*4+5", ["2", "3", "*", "4", "*", "5", "+"])
 	test1("2*3*4+5-10/3", ["2", "3", "*", "4", "*", "5", "+", "10", "3", "/", "-"])
+	test1("-3", ["0", "3", "-"])
+	test1("2** -3", ["2", "0", "3", "-", "**"])
+	test1("let x = 5", ["x", "let"])
 	test1Error(")", [])
 	test1Error("3*", [])
 	#inputFile = io.StringIO("2+2")
@@ -167,6 +267,6 @@ if __name__ == "__main__":
 			sys.stdout.write("\n")
 	sys.stdout.write("\033[m")
 	inputFile.seek(0)
-	for val in parse(scanner.tokenize(inputFile, env), OPL, env):
+	for val in parse(scanner.tokenize(inputFile, env), env):
 		print val,
 
